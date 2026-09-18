d3507f5fa2ce896c2b6739636dc2dc9983524643
Ailuropoda Melanoleuca <slendiocho@gmail.com>
2026-09-18 20:12:32 -0300

    arreglar todos los conflictos de la commit anterior

--- front/dist/js/dashbordsjs/adminregistrar.js
+++ front/dist/js/dashbordsjs/adminregistrar.js
@@ -16,22 +16,3 @@ document.querySelectorAll(".arrow-button").forEach(button => {
 
 });
 
-
-document.querySelectorAll(".arrow-button").forEach(button => {
-
-    button.addEventListener("click", function() {
-
-        setTimeout(() => {
-
-            const expanded =
-                this.getAttribute("aria-expanded");
-
-            this.innerHTML =
-                expanded === "true" ? "V" : "&lt;";
-
-        }, 50);
-
-    });
-
-});
-
